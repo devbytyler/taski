43a6139b6ebc24bce160aa5b4b235b9ede90b002
tylerstephens814 <tylerstephens814@gmail.com>
2017-03-17 17:54:12 -0600

    Pull project titles into into homepage via controller and erb

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get "contact", to: "pages#contact"
 
   resources :projects
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
